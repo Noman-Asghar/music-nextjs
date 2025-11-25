8c20a0b1712dcd3b56c0b74ea9bbb03a4d7b5ce8
Noman-Asghar <nomankk226@gmail.com>
2025-11-25 17:06:14 +0500

    update navbar code

--- components/ui/navbar-menu.tsx
+++ components/ui/navbar-menu.tsx
@@ -1,16 +1,14 @@
 "use client";
 import React from "react";
-import { motion } from "motion/react";
+import { motion, Transition } from "motion/react";
 
 
 
-const transition = {
+const transition: Transition = {
   type: "spring",
   mass: 0.5,
-  damping: 11.5,
+  damping: 12,
   stiffness: 100,
-  restDelta: 0.001,
-  restSpeed: 0.001,
 };
 
 export const MenuItem = ({
